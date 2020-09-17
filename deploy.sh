8beb98f802bf939ccee382e695e2857721a4f265
Jun Yu <shockz@me.com>
2020-09-18 01:34:36 +0900

    change address

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:shockzinfinity/shockzinfinity.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/shockzinfinity/shockz-vue-press.git master:gh-pages
+# git push -f https://github.com/shockzinfinity/shockz-vue-press.git master:gh-pages
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,11 @@
-const { description } = require("../../package");
+const { description } = require('../../package');
 
 module.exports = {
-  base: "/shockz-vue-press/",
+  base: '/',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: "ironpot devLog",
+  title: 'ironpot devLog',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
@@ -17,34 +17,28 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ["link", { rel: "icon", href: "/favicon.ico" }],
-    ["link", { rel: "manifest", href: "/manifest.json" }],
-    ["meta", { name: "theme-color", content: "#3eaf7c" }],
-    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
     [
-      "meta",
-      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
-    ],
-    [
-      "link",
-      { rel: "apple-touch-icon", href: "/icons/apple-touch-icon-152x152.png" },
-    ],
-    [
-      "link",
+      'link',
       {
-        rel: "mask-icon",
-        href: "/icons/safari-pinned-tab.svg",
-        color: "#3eaf7c",
+        rel: 'mask-icon',
+        href: '/icons/safari-pinned-tab.svg',
+        color: '#3eaf7c',
       },
     ],
     [
-      "meta",
+      'meta',
       {
-        name: "msapplication-TileImage",
-        content: "/icons/msapplication-icon-144x144.png",
+        name: 'msapplication-TileImage',
+        content: '/icons/msapplication-icon-144x144.png',
       },
     ],
-    ["meta", { name: "msapplication-TileColor", content: "#000000" }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
   ],
 
   /**
@@ -53,47 +47,47 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: "",
+    repo: '',
     displayAllHeaders: true,
     //activeHeaderLinks: false, // Default: true
     // editLinks: false,
     // editLinkText: "",
-    docsDir: "",
-    lastUpdated: "Last Updated",
+    docsDir: '',
+    lastUpdated: 'Last Updated',
     nav: [
       {
-        text: "tags",
-        link: "/tags",
+        text: 'tags',
+        link: '/tags',
       },
       {
-        text: "Tutorial",
-        link: "/tutorial/",
+        text: 'Tutorial',
+        link: '/tutorial/',
       },
       {
-        text: "devLog",
+        text: 'devLog',
         // link: "/dev-log/",
-        ariaLabel: "Developement Log",
+        ariaLabel: 'Developement Log',
         items: [
-          { text: "CentOS 8", link: "/dev-log/centos" },
-          { text: "Podman", link: "/dev-log/podman" },
-          { text: "Docker", link: "/dev-log/docker" },
-          { text: "Xwiki", link: "/dev-log/xwiki" },
-          { text: "Synology NAS", link: "/dev-log/synology" },
-          { text: "GitLab", link: "/dev-log/gitlab" },
-          { text: "SSL", link: "/dev-log/ssl" },
-          { text: "Nginx", link: "/dev-log/nginx" },
-          { text: ".net core", link: "/dev-log/dotnetcore" },
-          { text: "Konva Test", link: "/dev-log/konva" },
-          { text: "VuePress", link: "/dev-log/vuepress" },
-          { text: "mssql", link: "/dev-log/mssql" },
+          { text: 'CentOS 8', link: '/dev-log/centos' },
+          { text: 'Podman', link: '/dev-log/podman' },
+          { text: 'Docker', link: '/dev-log/docker' },
+          { text: 'Xwiki', link: '/dev-log/xwiki' },
+          { text: 'Synology NAS', link: '/dev-log/synology' },
+          { text: 'GitLab', link: '/dev-log/gitlab' },
+          { text: 'SSL', link: '/dev-log/ssl' },
+          { text: 'Nginx', link: '/dev-log/nginx' },
+          { text: '.net core', link: '/dev-log/dotnetcore' },
+          { text: 'Konva Test', link: '/dev-log/konva' },
+          { text: 'VuePress', link: '/dev-log/vuepress' },
+          { text: 'mssql', link: '/dev-log/mssql' },
         ],
       },
       {
-        text: "etc",
-        link: "/etc/",
+        text: 'etc',
+        link: '/etc/',
         items: [
-          { text: "개발환경설정", link: "/etc/devEnv" },
-          { text: "vscode-debugging", link: "/etc/vscode-debugging" },
+          { text: '개발환경설정', link: '/etc/devEnv' },
+          { text: 'vscode-debugging', link: '/etc/vscode-debugging' },
         ],
       },
     ],
@@ -112,12 +106,12 @@ module.exports = {
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
   plugins: [
-    "@vuepress/back-to-top",
-    "@vuepress/medium-zoom",
-    "@vuepress/google-analytics",
-    "@vuepress/nprogress",
+    '@vuepress/back-to-top',
+    '@vuepress/medium-zoom',
+    '@vuepress/google-analytics',
+    '@vuepress/nprogress',
     [
-      "@vuepress/pwa",
+      '@vuepress/pwa',
       {
         serviceWorker: true,
         updatePopup: true,
@@ -129,9 +123,9 @@ module.exports = {
     lineNumbers: true,
   },
 
-  ga: "UA-177405863-1",
+  ga: 'UA-177405863-1',
 
   locales: {
-    "/": { lang: "ko-KR" },
+    '/': { lang: 'ko-KR' },
   },
 };
