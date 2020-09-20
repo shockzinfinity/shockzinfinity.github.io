056f095d3ab0291a87d5f1cd0a8e174d18c62465
Jun Yu <shockz@shockz.io>
2020-09-20 20:58:20 +0900

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,7 +23,7 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'icon', href: '/img/icons/favicon.ico' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     [
       'meta',
@@ -32,23 +32,54 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '57x57', href: '/img/icons/apple-icon-57x57.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '60x60', href: '/img/icons/apple-icon-60x60.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '72x72', href: '/img/icons/apple-icon-72x72.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '76x76', href: '/img/icons/apple-icon-76x76.png' }],
+    [
+      'link',
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/img/icons/apple-icon-114x114.png' },
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/img/icons/apple-icon-120x120.png' },
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', sizes: '144x144', href: '/img/icons/apple-icon-144x144.png' },
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', sizes: '152x152', href: '/img/icons/apple-icon-152x152.png' },
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/img/icons/apple-icon-180x180.png' },
+    ],
     [
       'link',
       {
-        rel: 'mask-icon',
-        href: '/icons/safari-pinned-tab.svg',
-        color: '#3eaf7c',
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: '/img/icons/android-icon-192x192.png',
       },
     ],
     [
-      'meta',
-      {
-        name: 'msapplication-TileImage',
-        content: '/icons/msapplication-icon-144x144.png',
-      },
+      'link',
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/img/icons/favicon-32x32.png' },
     ],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    [
+      'link',
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/img/icons/favicon-96x96.png' },
+    ],
+    [
+      'link',
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/img/icons/favicon-16x16.png' },
+    ],
+    ['meta', { name: 'msapplication-TileColor', content: '#ffffff' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/img/icons/ms-icon-144x144.png' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
   ],
 
   /**
